4e350381deb0828ae6ebd79a7754c2b01b17bd2b
Saif Khayoon <88499318+SaifKhayoon@users.noreply.github.com>
2023-03-02 19:17:19 -0500

    renames

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,17 @@
 interface SunshineData {
-    lat: number;
-    lon: number;
-    sunshine: number;
-  }
-  
-  interface Color {
-    r: number;
-    g: number;
-    b: number;
-  }
-  
-  type ColorMap = { [key: number]: Color };
-  type Color = [number, number, number];
-  
-  
+  lat: number;
+  lon: number;
+  sunshine: number;
+}
+
+interface Color {
+  r: number;
+  g: number;
+  b: number;
+}
+
+type ColorMap = { [key: number]: Color };
+
+type ColorTuple = [number, number, number];
+
+type SunshineColorMap = { [key: number]: ColorTuple };
